a1ff7c05cf58a09a24d9c30aa4c58af64bb319c1
Priyanshi Bhatnagar <126306915+priyanshibhatn@users.noreply.github.com>
2023-12-21 15:35:11 +0530

    encode decode algo

--- app/src/main/java/com/example/crypticguardian/decode.java
+++ app/src/main/java/com/example/crypticguardian/decode.java
@@ -1,4 +1,97 @@
 package com.example.crypticguardian;
 
 public class decode {
-}
+
+        public static String dec(String s) {
+            String inv = "This code was not encrypted by CrypticGuardian";
+
+            // create the same initial
+            // string as in encode class
+            String initializer = "11111111";
+            Boolean flag = true;
+
+            // run a loop of size 8
+            for (int i = 0; i < 8; i++) {
+                // check if the initial value is same
+                if (initializer.charAt(i) != s.charAt(i)) {
+                    flag = false;
+                    break;
+                }
+            }
+            String data = "";
+
+            // reverse the encrypted code
+            for (int i = 8; i < s.length(); i++) {
+                char temp = s.charAt(i);
+                data = data.concat(String.valueOf(temp));
+            }
+
+            // create a 2 dimensional array
+            int asc[][] = new int[11101][8];
+            int idx = -1;
+            int idx2 = 0;
+
+            // run a loop of size of the encrypted code
+            for (int i = 0; i < data.length(); i++) {
+
+                // check if the position of the
+                // string if divisible by 7
+                if (i % 7 == 0) {
+                    // start the value in other
+                    // column of the 2D array
+                    idx++;
+                    idx2 = 0;
+                    char ch = data.charAt(i);
+                    asc[idx][idx2] = ch - '0';
+                    idx2++;
+                } else {
+                    // otherwise store the value
+                    // in the same column
+                    char temp = data.charAt(i);
+                    asc[idx][idx2] = temp - '0';
+                    idx2++;
+                }
+            }
+            // create an array
+            int num[] = new int[11111];
+            int nidx = 0;
+            int temp = 0;
+            int ct= 0;
+
+            // run a loop of size of the column
+            for (int i = 0; i <= idx; i++) {
+                ct = 0;
+                temp = 0;
+                // convert binary to decimal and add them
+                // from each column and store in the array
+                for (int j = 6; j >= 0; j--) {
+                    int temp1 = (int) Math.pow(2, ct);
+                    temp += (asc[i][j] * temp1);
+                    ct++;
+                }
+                num[nidx++] = temp;
+            }
+            String rv = "";
+            char ch;
+            // convert the decimal ascii number to its
+            // char value and add them to form a decrypted
+            // string using conception function
+            for (int i = 0; i < nidx; i++) {
+                ch = (char) num[i];
+                rv = rv.concat(String.valueOf(ch));
+            }
+
+
+            // check if the encrypted code was
+            // generated for this algorithm
+            if (data.length() % 7 == 0 && flag == true) {
+                // return the decrypted code
+                return rv;
+            } else {
+                // otherwise return an invalid message
+                return inv;
+            }
+        }
+    }
+
+
--- app/src/main/java/com/example/crypticguardian/encode.java
+++ app/src/main/java/com/example/crypticguardian/encode.java
@@ -1,4 +1,76 @@
 package com.example.crypticguardian;
 
 public class encode {
-}
+
+    public static String enc(String s) {
+            // create a string to add in the initial
+            // binary code for extra security
+            String initializer = "111111111";
+            int ct = 0;
+
+            // create an array
+            int a[] = new int[111111111];
+
+            // iterate through the string
+            for (int i = 0; i < s.length(); i++) {
+                // put the ascii value of
+                // each character in the array
+                a[i] = (int) s.charAt(i);
+                ct++;
+            }
+            String res = "";
+
+            // create another array
+            int bin[] = new int[111];
+            int idx = 0;
+
+            // run a loop of the size of string
+            for (int ii = 0; ii < ct; ii++) {
+
+                // get the ascii value at position
+                // i1 from the first array
+                int temp = a[ii];
+
+                // run the second nested loop of same size
+                // and set 0 value in the second array
+                for (int j = 0; j < ct; j++) bin[j] = 0;
+                idx = 0;
+
+                // run a while for temp > 0
+                while (temp > 0) {
+                    // store the temp module
+                    // of 2 in the 2nd array
+                    bin[idx++] = temp % 2;
+                    temp = temp / 2;
+                }
+                String dig = "";
+                String temps;
+
+                // run a loop of size 7
+                for (int j = 0; j < 7; j++) {
+
+                    // convert the integer to string
+                    temps = Integer.toString(bin[j]);
+
+                    // add the string using
+                    // concatenation function
+                    dig = dig.concat(temps);
+                }
+                String rev = "";
+
+                // reverse the string
+                for (int j = dig.length() - 1; j >= 0; j--) {
+                    char ca = dig.charAt(j);
+                    rev = rev.concat(String.valueOf(ca));
+                }
+                res = res.concat(rev);
+            }
+            // add the extra string to the binary code
+            res = initializer.concat(res);
+
+            // return the encrypted code
+            return res;
+        }
+    }
+
+
